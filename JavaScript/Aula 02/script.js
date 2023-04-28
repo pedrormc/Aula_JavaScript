ee3635536f70362e6c5666ee157abc7af7aa5913
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-04-28 11:32:20 -0300

    Aula 02
    alertas e tratamento de dados

---
+++ JavaScript/Aula 02/script.js
@@ -0,0 +1,11 @@
+var n1 = Number(window.prompt('Escreva um Número'))
+var n2 = Number(window.prompt('Escreva Outro Número'))
+// Number altera de "string" para "number"
+var s = (n1) + (n2)
+//( number + number ) = adição
+//( string + string ) = concatenação
+window.alert(`A Soma dos dois é:  ${s}  !!`)
+
+//----------------------------------------//
+
+var nome = document.querySelector('#nome')
