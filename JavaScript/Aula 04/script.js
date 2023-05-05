363c87a25126d83ce6f625264da32e0d0c68d0d3
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-05-05 14:50:28 -0300

    Imagem redonda
    esqueci de deixar a imagem redonda

--- JavaScript/Aula 04/script.js
+++ JavaScript/Aula 04/script.js
@@ -1,10 +1,14 @@
-/*var form_ok = window.prompt('Digite se é verdadeiro(true) ou falso(false)'); 
+/*
+var form_ok = window.prompt('Digite se é verdadeiro(true) ou falso(false)'); 
 if(form_ok== 'true') form_ok = true; else form_ok = false; if(form_ok) alert('Dados enviado com sucesso');
-else alert('Preenchimento incorreto')*/ 
+else alert('Preenchimento incorreto')
+*/ 
 
 
 form_ok = window.prompt('Digite sua idade');
 if(form_ok < 16)alert('Menor de idade, NÃO PODE VOTAR!');
 
 else if(form_ok <18 && 16)( alert('Votação optativa'));
-else alert('Maior de idade, VOTO OBRIGATÓRIO!');
+else alert('Maior de idade, VOTO OBRIGATÓRIO!');        
+
+// arrumei o códico que tu pediu pra achar o erro na sexta
