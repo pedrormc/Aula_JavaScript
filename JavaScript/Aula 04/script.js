9ea8937f67a3f803d7be6e00ec2963f77123704b
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-05-09 09:15:53 -0300

    Aula 04
    mudança dos elementos da página com o JavaScript

--- JavaScript/Aula 04/script.js
+++ JavaScript/Aula 04/script.js
@@ -1,14 +1,11 @@
-/*
-var form_ok = window.prompt('Digite se é verdadeiro(true) ou falso(false)'); 
-if(form_ok== 'true') form_ok = true; else form_ok = false; if(form_ok) alert('Dados enviado com sucesso');
-else alert('Preenchimento incorreto')
-*/ 
+function carregar(){
+    var msg = window.document.querySelector('#msg')
+      msg.innerHTML = 'tomi'
+
+      var img = window.document.querySelector('#img')
+      img.src = "img/img01.png"
 
 
-form_ok = window.prompt('Digite sua idade');
-if(form_ok < 16)alert('Menor de idade, NÃO PODE VOTAR!');
 
-else if(form_ok <18 && 16)( alert('Votação optativa'));
-else alert('Maior de idade, VOTO OBRIGATÓRIO!');        
+} 
 
-// arrumei o códico que tu pediu pra achar o erro na sexta
