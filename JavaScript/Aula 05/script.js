443828551bc20c5a2f73be326e8faf8010576e07
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-05-11 11:33:36 -0300

    mudando o background
    sla kajsdkjas

--- JavaScript/Aula 05/script.js
+++ JavaScript/Aula 05/script.js
@@ -42,26 +42,36 @@ function predict01(){
   var msg = window.document.querySelector('#msg')
       msg.innerHTML = 'VAI ESCOLHER O BOTÃO 01?'
 
-      }
+}
 
 
 function predict02(){
     var msg = window.document.querySelector('#msg')
         msg.innerHTML = 'VAI ESCOLHER O BOTÃO 02?'
       
-            }
+}
 
 function predict03(){
       var msg = window.document.querySelector('#msg')
           msg.innerHTML = 'VAI ESCOLHER O BOTÃO 03?'
             
-            }      
+}      
 
 // mudar a cor do site no js
 
 function fundo01(){
-  var back = window.document.querySelector('#body')
-     back = '#ff0000'
+  var back = window.document.querySelector('body')
+     back.style.backgroundColor = 'red'
+}
+
+function fundo02(){
+  var back = window.document.querySelector('body')
+     back.style.backgroundColor = 'blue'
+}
+
+function fundo03(){
+  var back = window.document.querySelector('body')
+     back.style.backgroundColor = 'green'
 }
 
 
