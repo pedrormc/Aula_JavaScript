c801fe4e2e4b9cb383c19966879d966def3d8504
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-05-11 10:07:54 -0300

    Aula 05
    estudos de funções com js

---
+++ JavaScript/Aula 05/script.js
@@ -0,0 +1,67 @@
+
+function carregar(){
+    var msg = window.document.querySelector('#msg')
+      msg.innerHTML = 'tomi'
+
+      var img = window.document.querySelector('#img')
+      img.src = "img/main.png"
+
+
+
+} 
+
+
+function muda01(){
+  var msg = window.document.querySelector('#msg')
+      msg.innerHTML = 'botão 01'
+
+      var img = window.document.querySelector('#img')
+      img.src = "img/img01.png"
+
+}
+function muda02(){
+  var msg = window.document.querySelector('#msg')
+      msg.innerHTML = 'botão 02'
+
+      var img = window.document.querySelector('#img')
+      img.src = "img/img02.png"
+
+}
+function muda03(){
+  var msg = window.document.querySelector('#msg')
+      msg.innerHTML = 'botão 03'
+
+      var img = window.document.querySelector('#img')
+      img.src = "img/img03.png"
+
+}
+
+// função do "hover no js"
+
+function predict01(){
+  var msg = window.document.querySelector('#msg')
+      msg.innerHTML = 'VAI ESCOLHER O BOTÃO 01?'
+
+      }
+
+
+function predict02(){
+    var msg = window.document.querySelector('#msg')
+        msg.innerHTML = 'VAI ESCOLHER O BOTÃO 02?'
+      
+            }
+
+function predict03(){
+      var msg = window.document.querySelector('#msg')
+          msg.innerHTML = 'VAI ESCOLHER O BOTÃO 03?'
+            
+            }      
+
+// mudar a cor do site no js
+
+function fundo01(){
+  var back = window.document.querySelector('#body')
+     back = '#ff0000'
+}
+
+
