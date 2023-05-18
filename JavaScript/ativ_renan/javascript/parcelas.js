4de7080c4496521edd376d030667d680da9f6965
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-05-18 08:37:36 -0300

    If Eslse
    if else pra saber se no parcelas.js a pessoa vai pagar a vista ou parcelado

--- JavaScript/ativ_renan/javascript/parcelas.js
+++ JavaScript/ativ_renan/javascript/parcelas.js
@@ -1,18 +1,42 @@
 //04
 var avst  = parseInt(prompt("Digite o valor do produto"))
-var juros = parseInt(prompt("Digite o número de parcelas"))
+
+window.alert("Qual será a forma de pagamento?")
+var forma_pag = prompt("Digite [1] para pagamento a vista,  ou [2] para parcelado")
+
 var valparse
 var total
 
-switch (juros) {
-    case 3:
-        total = avst + ((avst/100)*10)
-    break;
+if (forma_pag == 1) {
+    total = avst
 
-    case 5:
-        total = avst + ((avst/100)*20)
-    break;
+    window.alert(`O preço total é de R$${avst},00. obrigado pela preferência!!`)
+    
+} else {
+    var numparse = parseInt(prompt("Digite o número de parcelas de 3 a 5"))
+    switch (numparse) {
+        
+         case 3:
+            total = avst + ((avst/100)*10)
+        break;
+    
+        case 4:
+            total = avst + ((avst/100)*10)
+        break;
+    
+        case 5:
+            total = avst + ((avst/100)*20)
+        break;
+
+        default:
+         window.alert(`código inválido`)
+    
+    }
+    valparse = total/numparse
+
+    window.alert(`O preço a vista é de R$${avst},00 e parcelado em ${numparse}x vezes o total a pagar é de R$${total},00!!`)
+
+    window.alert(`O valor de cada parcela é de R$${valparse},00  agradecemos a preferência!!`)
 
 }
 
-window.alert(`preço a vista é de R$${avst},00 e parcelado em ${juros}x vezes o total a pagar é de R$${total},00`)
