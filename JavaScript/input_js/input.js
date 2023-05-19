f8e879a4fed433aac53e8294ff4a42c73a8f4d6d
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-05-19 10:50:05 -0300

    input com erro
    corrigir erro da porra do input que tá indo errado

---
+++ JavaScript/input_js/input.js
@@ -0,0 +1,9 @@
+function submit() {
+let fname = document.querySelector('#fname')
+let idade = document.querySelector('#idade')
+
+document.querySelector('#resposta1').innerHTML = fname
+
+document.querySelector('#resposta2').innerHTML = idade
+
+}
