dfb76a365fdee58562183c11396bb22f5113db2d
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-08-18 15:04:51 -0300

    quase lá
    quase terminando a integração da api com o front end

--- Crud/fdss/frontend/src/App.jsx
+++ Crud/fdss/frontend/src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import {toast , ToastCointainer} from "react-toastify"
+import styled from "styled-components"
+import Form from "./components/Form.js"
+
+
+const Container = styled.div`
+width 100%;
+max-widith: 800px;
+margin-top : 20px;
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 10px;
+`
+
 
 function App() {
-  
+  return(
+    <>
 
-  return <div className ="App"></div>
+<Container>
+  <Title>USUÁRIOS</Title>
+</Container>
+
+    </>
+  )
 }
 
 export default App
