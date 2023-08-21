f60e0c7ea3badd8ea81abc911ee48c0aa3ceae64
pedrormc <113632337+pedrormc@users.noreply.github.com>
2023-08-21 03:33:01 -0300

    aprendendo react
    bora que bora, api montada full agr é só aprender a aplicar os components direito

--- Crud/caraio/frontend/src/App.js
+++ Crud/caraio/frontend/src/App.js
@@ -1,7 +1,7 @@
 import GlobalStyle from "./styles/global";
 import styled from "styled-components";
-import Form from "./components/Form.jsx";
-//import Grid from "./components/Grid";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid";
 import { useEffect, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
